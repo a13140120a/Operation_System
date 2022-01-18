a21369712c949555fad527d119a602d3be273442
a13140120a <71315973+a13140120a@users.noreply.github.com>
2022-01-18 11:35:46 +0800

    Add files via upload

---
+++ linux_create_process_example.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/wait.h"
+int main()
+{
+	int A;
+	A=fork();
+	int status;
+	if(A==0)
+	{
+		printf("this is from child process\n");
+		execlp("/bin/ls","ls",NULL);
+
+	}else{
+		printf("this is from parent process\n");
+		int pid = wait(&status);
+		printf("child %d complete",pid);
+	}
+
+	printf("process end %d\n",A);
+
+	return 0;
+}
