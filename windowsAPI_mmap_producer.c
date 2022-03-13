45b0ad372e9366ae2a29eb30bf82edb171fd521f
a13140120a <71315973+a13140120a@users.noreply.github.com>
2022-03-13 19:02:48 +0800

    Update windowsAPI_mmap_producer.c

--- windowsAPI_mmap_producer.c
+++ windowsAPI_mmap_producer.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[])
       OPEN ALWAYS, /* open new or existing file */
       FILE ATTRIBUTE NORMAL, /* routine file attributes */
       NULL); /* no file template */
-
+    /* 建立SharedObject的命名共用記憶體物件(named shared-memory object) */
     hMapFile = CreateFileMapping(hFile, /* file handle */
       NULL, /* default security */
       PAGE READWRITE, /* read/write access to mapped pages */
       0, /* map entire file */
       0,
       TEXT("SharedObject")); /* named shared memory object */
-
+    /* 建立共用記憶體物件的指標 */
     lpMapAddress = MapViewOfFile(hMapFile, /* mapped object handle */
       FILE MAP ALL ACCESS, /* read/write access */
       0, /* mapped view of entire file */
