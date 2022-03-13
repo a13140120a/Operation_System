4c7ce9cef11bd42a4a6906514759b3e9246c872a
a13140120a <71315973+a13140120a@users.noreply.github.com>
2022-03-13 18:55:14 +0800

    Add files via upload

---
+++ windowsAPI_mmap_consumer.c
@@ -0,0 +1,23 @@
+#include <windows.h>
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    HANDLE hMapFile;
+    LPVOID lpMapAddress;
+
+    hMapFile = OpenFileMapping(FILE MAP ALL ACCESS, /* R/W access */
+      FALSE, /* no inheritance */
+      TEXT("SharedObject")); /* name of mapped file object */
+
+    lpMapAddress = MapViewOfFile(hMapFile, /* mapped object handle */
+      FILE MAP ALL ACCESS, /* read/write access */
+      0, /* mapped view of entire file */
+      0,
+      0);
+
+    /* read from shared memory */
+    printf("Read message %s", lpMapAddress);
+
+    UnmapViewOfFile(lpMapAddress);
+    CloseHandle(hMapFile);
+}
